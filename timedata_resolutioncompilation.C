baa75e167053b683b603b4ca5458557caf0d4e92
Thakur Bhatta <bhattathakur2015@gmail.com>
2018-11-08 19:00:43 -0600

    working on the timedata

--- timedata_resolutioncompilation.C
+++ timedata_resolutioncompilation.C
@@ -4,13 +4,13 @@ void timedata_resolutioncompilation()
   //Data file storing input file and output file
   const char * inputfile="FINAL/datetimeruntime.dat";
   const char * inputresolution="FINAL/allrandomdata.dat";
-  const char * outfile="FINIL/compiledtimeresolution.dat";
+  const char * outfile="FINAL/compiledtimeresolution.dat";
 
   //Check if inputfile is opened successfully
   //Defining the variables to store date and time
   Int_t date,tim,nanosec=0;
   Double_t runtime,resolution,error_resolution;
-  TTimeStamp timestamp;
+  // TTimeStamp timestamp=new TTimeStamp();
   
   //Base time to subtract from
   TTimeStamp basetime(20180901,000000,0,kTRUE,-6*3600);
@@ -33,10 +33,11 @@ void timedata_resolutioncompilation()
 	    cout<<"Unable to open the file "<<inputresolution<<endl;
 	    return 0;
 	  }
+	cout<<"-----------------------------------"<<endl;
 	//Manipulating the data
 	while(1)
 	  {
-	    cout<<"###################################"<<endl;
+	    
 	    datainput>>resolution>>error_resolution;//Reading resolution and errro
 	    datain>>date>>tim>>runtime; //
 	   
@@ -46,18 +47,18 @@ void timedata_resolutioncompilation()
 	    cout<<resolution<<'\t'<<error_resolution<<endl;
 	    cout<<"original output from time file ="<<endl;
 	    cout<<date<<'\t'<<tim<<'\t'<<runtime<<'\t'<<endl;
-	    // timestamp=new TTimeStamp(date,time,nanosec,kTRUE,0);
-	    timestamp.Set(date,time,nanosec,kTRUE,-6*3600);//offset between CDT and UTC = 6hrs
+	   auto timestamp=new TTimeStamp(date,tim,nanosec,kTRUE,0);
+	    // timestamp.Set(date,time,nanosec,kTRUE,-6*3600);//offset between CDT and UTC = 6hrs
 	    //cout<<"time = ";
 	    //cout<<timestamp.Print();
 	    // cout<<"month = "<< timestamp.GetMonth()<<endl;
 	    //  cout<<"Date  = "<< timestamp.GetDate()<<endl;
 	    // cout<<"Time  = "<<timestamp.GetTime()<<endl;
-	    cout<<"Second ="<<timestamp.GetSec()<<endl;
-	    cout<<"NanoSecond="<<timestamp.GetNanoSec()<<endl;
-	    cout<<"As Double ="<<timestamp.AsDouble()<<endl;
-	    cout<<"As String ="<<timestamp.AsString()<<endl;
-	    double newtime=timestamp.GetSec()+runtime/2;
+	    cout<<"Second ="<<timestamp->GetSec()<<endl;
+	    cout<<"NanoSecond="<<timestamp->GetNanoSec()<<endl;
+	    cout<<"As Double ="<<timestamp->AsDouble()<<endl;
+	    cout<<"As String ="<<timestamp->AsString()<<endl;
+	    double newtime=timestamp->GetSec()+runtime/2;
 	    cout<<"run time ="<<runtime<<endl;
 	    cout<<"new time ="<<newtime<<endl;
 	    //Storing the output in the file
@@ -68,17 +69,21 @@ void timedata_resolutioncompilation()
 		{
 		  cout<<"successfully opened the file "<<outfile<<endl;
 		  cout<<setw(10)<<newtime-basetimesec<<setw(15)<<resolution<<setw(10)<<runtime/2<<setw(10)<<error_resolution<<endl; //newtime-basetime
-		  timereso<<setw(10)<<newtime-basetimesec<<setw(15)<<resolution<<setw(10)<<runtime/2<<setw(10)<<error_resolution<<endl;
+		  timereso<<setw(10)<<newtime-basetimesec<<setw(15)<<2.35*resolution<<setw(10)<<runtime/2<<setw(10)<<2.35*error_resolution<<endl;//Multiply by 2.35
 		}
 	    else
 		{
 		  cout<<"Error opening the file "<<outfile<<endl;
 		}
+	     cout<<"-----------------------------------"<<endl;
+	  cout<<endl;
 	  }
+	 
     }
   else
     {
 	cout<<"failed to open the file" <<inputfile<<endl;
 	return 0;
     }
+
 }
--- timeresolution.C
+++ timeresolution.C
@@ -1,39 +1,40 @@
-void timeresolution()
+timeresolution()
 {
-  const char * pdfetrue="FINAL/resolutionvstime.pdf"; //pdf file location
-  const char * inputdata="DATA/compiledtimeresolution.dat"; //source for data file
+   // const char * filedata="DATA/bkg0E_estandE_true.dat"; //File storing the estimated and true energy and their errors
+  // const char * rootfile="ROOTFILES/bkg0true_estimated_enregyplot.root"; //root file to save the plot
+  // const char * filenamee="DATA/bkg0slope_intercept.dat"; //file to store slope and intercept of plot
+  const char * pdfetrue="FINAL/resolutionvstimesecond.pdf"; //pdf file location
+  char * inputdata="FINAL/compiledtimeresolution.dat";
+  auto c=new TCanvas();
+  c->SetGrid();
+  c->SetFillColor(20);
+  graph_expected=new TGraphErrors(inputdata,"%lg%lg%lg%lg","");//time, resolution, runtime/2,error in resolution
+  graph_expected->SetTitle("FWHM vs Time  Plot;time (s); FWHM (eV);");
+  graph_expected->GetYaxis()->SetTitleOffset(1.2);
+  graph_expected->GetXaxis()->SetTitleOffset(1.2);
+  graph_expected->SetMarkerColor(4);
+  graph_expected->SetMarkerStyle(21);
+  graph_expected->SetLineColor(9);
+  graph_expected->SetLineWidth(2);
+  graph_expected->Draw("AP");
+  graph_expected->Fit("pol0");
+  // graph_expected->GetFunction("pol0")->SetLineStyle(2);
+  graph_expected->GetFunction("pol0")->SetParName(0,"FWHM");
+  // graph_expected->GetFunction("pol0")->SetParName(1,"Slope(P_{1})");
 
-  
-  atuo resocan=new TCanvas();
-  resocan->SetGrid();
-  resocan->SetFillColor(20);
-  graph=new TGraphErrors(inputdata,"%lg%lg%lg%lg","");//time, resolution, runtime/2,error in resolution
-  graph->SetTitle("Resolution vs Time  Plot;time (s); resolution (eV);");
-  graph->GetYaxis()->SetTitleOffset(1.2);
-  graph->GetXaxis()->SetTitleOffset(1.2);
-  graph->SetMarkerColor(4);
-  graph->SetMarkerStyle(21);
-  graph->SetLineColor(9);
-  graph->SetLineWidth(2);
-  graph->Draw("AP");
-  graph->Fit("pol1");
-  graph->GetFunction("pol1")->SetLineStyle(2);
-  graph->GetFunction("pol1")->SetParName(0,"Intercept(P_{0})");
-  graph->GetFunction("pol1")->SetParName(1,"Slope(P_{1})");
-
-  resocan->GetFrame()->SetFillColor(21);
-  resocan->GetFrame()->SetBorderSize(12);
+  c->GetFrame()->SetFillColor(21);
+  c->GetFrame()->SetBorderSize(12);
   gStyle->SetStatX(0.9);
   gStyle->SetStatY(0.92);
   gStyle->SetOptFit();
-  resocan->Update();
-  resocan->SaveAs(pdfetrue);
+  c->Update();
+  c->SaveAs(pdfetrue);
   
   /*//Legend
   auto legend=new TLegend(0.1,0.8,0.3,0.9);//x1,y1,x2,y2
   //legend->SetHeader("The Legend Title");
-  legend->AddEntry(graph->GetFunction("pol1"),"E_{estimated}=P_{0}+P_{1}E_{true}","l");
-  legend->AddEntry(graph,"Graph with error bars","lep");//line,errorbars,polymarker
+  legend->AddEntry(graph_expected->GetFunction("pol0"),"E_{estimated}=P_{0}+P_{1}E_{true}","l");
+  legend->AddEntry(graph_expected,"Graph with error bars","lep");//line,errorbars,polymarker
   gStyle->SetLegendFont(12);
   gStyle->SetLegendFillColor(7);
   legend->Draw();
@@ -42,13 +43,13 @@ void timeresolution()
   file=new TFile(rootfile,"RECREATE");
   if(file->IsOpen())cout<<rootfile <<" opened successfully"<<endl;
   c->SaveAs(pdfetrue);
-  graph->Write();
+  graph_expected->Write();
   c->Write();
   //c->Close();
   ofstream outputgraph(filenamee);
   if(outputgraph.is_open())
    {
-     outputgraph<<setw(10)<<  graph->GetFunction("pol1")->GetParameter(0)<<setw(10)<<graph->GetFunction("pol1")->GetParameter(1)<<endl;
+     outputgraph<<setw(10)<<  graph_expected->GetFunction("pol0")->GetParameter(0)<<setw(10)<<graph_expected->GetFunction("pol0")->GetParameter(1)<<endl;
      cout<<"Successfully stored intercept and slope in the file"<<filenamee<<endl;
    }
  else
